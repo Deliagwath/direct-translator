e0a3ba7f130e6ecb72de52af42a2ca7e0d4c6c53
Deliagwath <PPL3000@Gmail.com>
2020-07-14 00:51:07 +0800

    Updates

---
+++ precache-manifest.662d6dabd67ac6962661cbba81ec60d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753f27ac310560717534625f6934b34a",
+    "url": "/direct-translator/index.html"
+  },
+  {
+    "revision": "c78a3635a2e32284b3b3",
+    "url": "/direct-translator/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e2f5038caebdeda2a890",
+    "url": "/direct-translator/static/js/2.16802d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/direct-translator/static/js/2.16802d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78a3635a2e32284b3b3",
+    "url": "/direct-translator/static/js/main.de544fc5.chunk.js"
+  },
+  {
+    "revision": "a1cff9886d48af5987b0",
+    "url": "/direct-translator/static/js/runtime-main.63d5c03a.js"
+  }
+]);
